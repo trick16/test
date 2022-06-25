57d760b8ac5214628b5eeba8167c19187df3a3b2
Yuliia <julim1603@gmail.com>
2022-06-25 13:53:59 +0300

    start utils

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Customch from "./pages/Customch";
 import Threech from "./pages/Threech";
+import TestUtils from "./pages/TestUtils";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path="three" element={<Threech />} />
         <Route path="custom" element={<Customch />} />
+        <Route path="utils" element={<TestUtils />} />
       </Routes>
     </>
   );
---
+++ src/pages/TestUtils.js
@@ -0,0 +1,4 @@
+const TestUtils = (props) => {
+  return <div></div>;
+};
+export default TestUtils;

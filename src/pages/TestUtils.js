0394da8d77dbb0d78fe35816ffdb3efa23cd7d10
Yuliia <julim1603@gmail.com>
2022-06-26 16:51:52 +0300

    array functions

--- src/pages/TestUtils.js
+++ src/pages/TestUtils.js
@@ -1,4 +1,17 @@
+import { chunk, compact, difference } from "../utils";
 const TestUtils = (props) => {
-  return <div></div>;
+  const cleanArr = compact([0, 1, false, 2, "", 3]);
+  const diff = difference([2, 1, 3, 4], [2, 3, 0, 2]);
+  const part = chunk([0, 1, 2, 3, 4, 5, 6], 2);
+  console.log(part);
+  return (
+    <div>
+      {cleanArr}
+      <br />
+      {diff}
+      <br />
+      {part}
+    </div>
+  );
 };
 export default TestUtils;
--- src/utils/array.js
+++ src/utils/array.js
@@ -1,1 +1,32 @@
-export const compact = () => {};
+export const isEmpty = (item) => {
+  if (item) {
+    return true;
+  }
+  return false;
+};
+
+export const compact = (arr) => {
+  const result = arr.filter((item) => !!item);
+  return result;
+};
+
+export const chunk = (array, size, result = []) => {
+  if (array.length > 0) {
+    const part = array.splice(0, size);
+    result.push(part);
+
+    chunk(array, size, result);
+  }
+  return result;
+};
+
+export const difference = (checkArr, values) => {
+  const result = [];
+  for (let item in checkArr) {
+    const itemCheck = checkArr[item];
+    if (!values.includes(itemCheck)) {
+      result.push(itemCheck);
+    }
+  }
+  return result;
+};

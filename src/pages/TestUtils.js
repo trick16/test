839e38f3b05699098024c1d0bf18fb5a14f6ddd4
Yuliia <julim1603@gmail.com>
2022-06-26 22:06:07 +0300

    array flatten

--- src/pages/TestUtils.js
+++ src/pages/TestUtils.js
@@ -4,7 +4,6 @@ const TestUtils = (props) => {
   const diff = array.difference([2, 1, 3, 4], [2, 3, 0, 2]);
   const part = array.chunk([0, 1, 2, 3, 4, 5, 6], 3);
   const flat = array.flatten([1, [2, [3, [4]], 5]]);
-  console.log(flat);
   return (
     <div>
       {cleanArr}
